9212a0ae66209694e0ca4ec7d3291bb892318b87
xtensa <xtensa@users.noreply.github.com>
2008-04-22 10:11:17 +0000

    Some cleanups and code optimizations were made.

--- firmware/gLCD.c
+++ firmware/gLCD.c
@@ -33,36 +33,40 @@
 #include "gLCD.h"
 #include "fonts.h"
 
+
+bool is_lcd_on=false;
 uint8_t gLCD_x, gLCD_y;
 
 FILE gLCD_str5x7   = FDEV_SETUP_STREAM(gLCD_putchar_5x7,   NULL, _FDEV_SETUP_WRITE);
 FILE gLCD_str16x24 = FDEV_SETUP_STREAM(gLCD_putchar_16x24, NULL, _FDEV_SETUP_WRITE);
 
 
-void gLCD_init()
+void gLCD_switchon()
 {
+	if (is_lcd_on) return;
 	// disabling JTAG if enabled (should be written twice) 
 	MCUCSR |= _BV(JTD);
 	MCUCSR |= _BV(JTD);
 	_delay_us(10); //??
 
-	// turn off display power
-	B02_PORT_UNSET(LCD_POWER);
-
 	// setting data direction for output ports
 	B02_DDR(LCD_POWER)  |= _BV(B02_LCD_POWER);
 
+	// turn off display power
+	B02_PORT_SET(LCD_POWER);
+
 	DDR(S6B0108_PCMD)  |=  ( _BV(CS1) | _BV(CS2) | _BV(RST) | _BV(E) | _BV(RW) | _BV(RS));
 
-	PORT(S6B0108_PCMD) &= ~( _BV(CS1) | _BV(CS2) |_BV(RW) | _BV(RS) | _BV(RST) | _BV(E));
+	PORT(S6B0108_PCMD) &= ~( _BV(CS1) | _BV(CS2) | _BV(RW) | _BV(RS) | _BV(RST) | _BV(E));
 	
 	// swithing display power back after delay
 	_delay_us(5);
-	B02_PORT_SET(LCD_POWER);
+	B02_PORT_UNSET(LCD_POWER);
 	// wait 2us and bring power up ( 1us according to datasheet)
 	_delay_us(2);
 	PORT(S6B0108_PCMD) |= _BV(RST);
 
+	is_lcd_on=true;
 	// FIXME: first screen clearence should be done twice
 	//        For unkown reason first cls leave garbage on screen
 	gLCD_cls();
@@ -73,10 +77,21 @@ void gLCD_init()
 	init_font16x24();
 }
 
+void gLCD_switchoff()
+{
+	if (!is_lcd_on) return;
+        PORT(S6B0108_PCMD) &= ~( _BV(CS1) | _BV(CS2) |_BV(RW) | _BV(RS) | _BV(RST) | _BV(E));
+	PORT(S6B0108_PDATA)=0x00;
+	B02_PORT_SET(LCD_POWER); 
+	is_lcd_on=false;
+}
+
 
 void gLCD_cls()
 {
 	uint8_t i,j;
+
+	if (!is_lcd_on) return;
 #if GLCD_RES_X/64==1
 	PORT(S6B0108_PCMD) |= _BV(CS1);
 #endif
@@ -116,6 +131,7 @@ void gLCD_draw_rect(uint16_t x1, uint8_t y1, uint16_t x2, uint8_t y2, uint8_t pt
 	uint8_t  cs, y, pixs[64], pat, pattern=ptrn, width=x2-x1+1;
 	uint16_t x, xt; 
 
+	if (!is_lcd_on) return;
 
 	x2 = MIN(x2,GLCD_RES_X-1);
 	y2 = MIN(y2,GLCD_RES_Y-1);
@@ -218,6 +234,7 @@ void gLCD_draw_rect(uint16_t x1, uint8_t y1, uint16_t x2, uint8_t y2, uint8_t pt
 void gLCD_pixel(uint16_t x, uint8_t y, bool onoff)
 {
 	uint8_t tmp;
+	if (!is_lcd_on) return;
 	if(x>=GLCD_RES_X || y>=GLCD_RES_Y) return;
 	if(x/64==0) PORT(S6B0108_PCMD) |= _BV(CS1); else PORT(S6B0108_PCMD) &= ~_BV(CS1); 
 #if GLCD_RES_X/64>1
@@ -246,6 +263,7 @@ void gLCD_line(uint16_t x1, uint8_t y1, uint16_t x2, uint8_t y2, bool onoff)
 	int8_t j;
 	int16_t x,y;
 	
+	if (!is_lcd_on) return;
 	if(x1>=GLCD_RES_X || y1>=GLCD_RES_Y) return;
 	if(x2>=GLCD_RES_X || y2>=GLCD_RES_Y) return;
 	
@@ -282,6 +300,7 @@ void gLCD_line(uint16_t x1, uint8_t y1, uint16_t x2, uint8_t y2, bool onoff)
 void gLCD_frame(uint16_t x1, uint8_t y1, uint16_t x2, uint8_t y2, uint8_t width, bool onoff)
 {
 	uint8_t i;
+	if (!is_lcd_on) return;
 	/*
 	 * First we shoud swap the coordinats so that we have
 	 * top left corner and right bottom corner vertices.
@@ -383,6 +402,7 @@ void gLCD_rwtest()
 int gLCD_putchar(char c, const uint8_t **font, uint8_t width, uint8_t height, uint8_t space)
 {
 	uint8_t zero=0;
+	if (!is_lcd_on) return 0;
 	gLCD_draw_img_progmem(gLCD_x,gLCD_y,font[(uint8_t)c],width,height);
 	if(space) gLCD_draw_img(gLCD_x+width,gLCD_y,&zero,space,height);
 
